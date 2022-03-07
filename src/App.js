44972cd8bd26de440538e7badc1b21b9dbb192aa
Marco Luciano <maurelio.sluciano@gmail.com>
2022-03-07 17:09:17 -0300

    cleaning codes

--- src/App.js
+++ src/App.js
@@ -1,23 +1,9 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import Container from "./components/Container";
 import Card from './components/Card';
 
 
 const App = () => {
-
-// const [text, setText] = useState([]);
-
-// const fetchAdvice = async () => {
-//     const res = await fetch('https://api.adviceslip.com/advice')
-//     const data = await res.json()
-//     console.log(data)
-//     setText(data.slip)
-// }
-
-// useEffect(() => {
-//     fetchAdvice()
-// }, [])
-
 
 return (
     <Container>
